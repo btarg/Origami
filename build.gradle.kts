7a54eabbe85bd9eb275b4bafbac3bf1692d9df8c
iCrazyBlaze <icrazyblaze1@gmail.com>
2023-09-28 22:11:35 +0100

    Run-paper plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,63 +2,33 @@ plugins {
     `java-library`
     `maven-publish`
     id("com.github.johnrengelman.shadow") version "8.1.1"
-    id("java")
+    id("io.papermc.paperweight.userdev") version "1.5.5"
+    id("xyz.jpenilla.run-paper") version "2.2.0" // Adds runServer and runMojangMappedServer tasks for testing
 }
-
-
 repositories {
     mavenCentral()
     google()
     maven("https://repo.papermc.io/repository/maven-public/")
-
-    maven {
-        url = uri("https://oss.sonatype.org/content/groups/public/")
-    }
-
+    maven("https://oss.sonatype.org/content/groups/public/")
 }
-
 dependencies {
+    paperweight.paperDevBundle("1.20.1-R0.1-SNAPSHOT")
     api("commons-io:commons-io:2.13.0")
     compileOnly("org.projectlombok:lombok:1.18.28")
     annotationProcessor("org.projectlombok:lombok:1.18.28")
     compileOnly("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT")
-
 }
-
 group = "io.github"
 version = "1.0-SNAPSHOT"
 description = "Origami"
 java.sourceCompatibility = JavaVersion.VERSION_17
-
-
-tasks {
-    register("copyJar") {
-        dependsOn(shadowJar)
-        doLast {
-            val outputDir = "E:/MINECRAFT TEST SERVER/plugins"
-            val outputJarName = "${project.description}-${project.version}.jar"
-
-            val outputJarFile = File(outputDir, outputJarName)
-            val shadowJarFile = shadowJar.get().archiveFile.get().asFile
-
-            outputJarFile.parentFile.mkdirs()
-            shadowJarFile.copyTo(outputJarFile, true)
-
-            println("Shaded JAR copied to: $outputJarFile")
-        }
-    }
-}
-
-fun executeCopyJarTask(project: Project) {
-    val copyJarTask = project.tasks.getByName("copyJar")
-    copyJarTask.actions.forEach { action -> action.execute(copyJarTask) }
-}
-
 tasks {
     assemble {
+        dependsOn(reobfJar)
         dependsOn(shadowJar)
-        doLast {
-            executeCopyJarTask(project)
-        }
+    }
+    runServer {
+        dependsOn(assemble)
+        minecraftVersion("1.20.1")
     }
 }
