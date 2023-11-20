a7b220a4d277fcf455d6c4c48a8ba7a5853247fe
btarg <icrazyblaze1@gmail.com>
2023-11-20 01:29:27 +0000

    Update RootCommand.java

--- src/main/java/io/github/btarg/commands/RootCommand.java
+++ src/main/java/io/github/btarg/commands/RootCommand.java
@@ -13,6 +13,7 @@ import io.github.btarg.registry.RegistryHelper;
 import io.github.btarg.resourcepack.ResourcePackGenerator;
 import io.github.btarg.util.blocks.BlockPos;
 import net.kyori.adventure.text.Component;
+import org.apache.commons.lang3.StringUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Chunk;
@@ -52,26 +53,26 @@ public class RootCommand implements TabExecutor {
             }
         } else if (Objects.equals(args[0], "reload")) {
 
-            if (Objects.equals(args[1], "blocks")) {
+            if (StringUtils.equalsAny(args[1], "blocks", "all")) {
                 sender.sendMessage("Reloading custom blocks...");
                 CustomBlockRegistry.ClearBlockRegistry();
                 OrigamiMain.definitionSerializer.loadAndRegister(sender, CustomBlockDefinition.class);
-            } else if (Objects.equals(args[1], "items")) {
+            }
+            if (StringUtils.equalsAny(args[1], "items", "all")) {
                 sender.sendMessage("Reloading custom items...");
                 CustomItemRegistry.ClearItemRegistry();
                 OrigamiMain.definitionSerializer.loadAndRegister(sender, CustomItemDefinition.class);
-            } else if (Objects.equals(args[1], "recipes")) {
+            }
+            if (StringUtils.equalsAny(args[1], "recipes", "all")) {
                 sender.sendMessage("Reloading custom recipes...");
                 CustomRecipeRegistry.ClearRecipeRegistry();
                 OrigamiMain.definitionSerializer.loadAndRegister(sender, CustomRecipeDefinition.class);
-            } else if (Objects.equals(args[1], "resources")) {
+            }
+            if (StringUtils.equalsAny(args[1], "resources", "all")) {
                 sender.sendMessage("Reloading resource pack...");
                 // Generate resource pack and serve with http
                 ResourcePackListener.serveResourcePack(ResourcePackGenerator.generateResourcePack());
                 Bukkit.getServer().getOnlinePlayers().forEach(ResourcePackListener::sendResourcePack);
-
-            } else {
-                sender.sendMessage(ChatColor.RED + "Please specify which registry you want to reload.");
             }
         } else if (Objects.equals(args[0], "listblocks")) {
 
@@ -90,7 +91,7 @@ public class RootCommand implements TabExecutor {
                     sender.sendMessage(String.valueOf(count) + finalString);
 
                 } else {
-                    sender.sendMessage("No blocks in database!");
+                    sender.sendMessage("No blocks in this chunk!");
                 }
             }
             return true;
@@ -126,7 +127,7 @@ public class RootCommand implements TabExecutor {
             }
 
         } else if (Objects.equals(args[0], "reload")) {
-            tabComplete = Arrays.asList("blocks", "items", "recipes", "resources");
+            tabComplete = Arrays.asList("all", "blocks", "items", "recipes", "resources");
         }
 
         return tabComplete;
