96b32ff348eb51ea706524a611b645542e65a468
btarg <icrazyblaze1@gmail.com>
2023-11-22 17:40:58 +0000

    fix error when custom content folder not present

--- src/main/java/io/github/btarg/util/ContentPackHelper.java
+++ src/main/java/io/github/btarg/util/ContentPackHelper.java
@@ -1,7 +1,6 @@
 package io.github.btarg.util;
 
 import io.github.btarg.OrigamiMain;
-import lombok.Getter;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
@@ -11,12 +10,24 @@ import java.util.Objects;
 
 public class ContentPackHelper {
 
-    @Getter
-    private static final File contentPacksFolder = new File(OrigamiMain.getInstance().getDataFolder(), "custom");
-
-    public static File[] getAllContentPacks() throws IOException {
-        FileUtils.createParentDirectories(contentPacksFolder);
-        return Arrays.stream(Objects.requireNonNull(contentPacksFolder.listFiles())).filter(File::isDirectory).toArray(File[]::new);
+    public static File getContentPacksFolder() {
+        File folder = new File(OrigamiMain.getInstance().getDataFolder(), "custom");
+        try {
+            FileUtils.createParentDirectories(folder);
+            if (!folder.isDirectory()) {
+                // If it's not a directory, attempt to delete the file and recreate as a directory
+                folder.delete();
+                folder.mkdirs();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return folder;
     }
 
+
+    public static File[] getAllContentPacks() {
+        return Arrays.stream(Objects.requireNonNull(getContentPacksFolder().listFiles(File::isDirectory))).toArray(File[]::new);
+    }
 }
+
